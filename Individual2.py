15ca82e7ee2c345c7feade84252dd0535fa59bb7
dibovdmitry <mega.dmitry2002@mail.ru>
2022-05-16 11:58:08 +0300

    add changes for Individual2.py

---
+++ Individual2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Создать абстрактный базовый класс Body (тело) с абстрактными функциями вычисления
+площади поверхности и объема. Создать производные классы: Parallelepiped
+(параллелепипед) и Ball (шар) со своими функциями площади поверхности и объема.
+"""
+
+
+class Body:
+
+    def surface_area(self):
+        print("")
+
+
+class Parallelepiped(Body):
+
+    def volume(self):
+        print("")
